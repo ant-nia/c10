91f7179d73708aeefa3f866adf62b6e7dc736488
antonia labra barraza <antonialabrabarraza@uc.cl>
2025-10-24 11:45:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 const portfolio = document.querySelector("#portafolio");
 
-async function datos(url) {
+async function datos(raw) {
     try {
-         let consulta = await fetch(raw);
+        let consulta = await fetch(raw);
         let resultado = await consulta.json();
         let trabajos = resultado.data;
         console.log(trabajos);
         trabajos.forEach((trabajo) => {
             portfolio.innerHTML += `
-                <div class="col">
-                    <div class="card shadow-sm h-100">
-                        <img src="${v.photo}" class="card-img-top" alt="${v.title}">
-                        <div class="card-body d-flex flex-column">
-                            <h5 class="card-title">${v.title}</h5>
-                            <p class="flex-grow-1">${v.description}</p>
-                            <div class="d-flex justify-content-between align-items-center mt-auto">
+
+                            <div class="col">
+                                <div class="card shadow-sm">
+                                <img src="${trabajo.imagen}" class"card-img-top">
+                                <div class="card-body">
+                                <p class="card-text">${trabajo.titulo}</p>
+                                <div class="d-flex
+                                justify-content-between
+                                align-items-center">
                                 <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">${v.category}</button>
+                                <button type="button" class="btn btn-sm btn-outline-secondary">${trabajo.categoria}</button>
                                 </div>
-                                <span class="date-badge">${v.date}</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>`;
+                                <small class="text-body-secondary"> ☆ ☆ ☆</small>
+             </div>
+             </div>
+            </div>
+            </div>`;
         });
     } catch (error) {
         console.error("Error al cargar los datos:", error);
